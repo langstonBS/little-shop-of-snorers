c73cd8f0e5b625cce06daf23ba615a97e43fc4e0
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-24 16:17:59 -0700

    buss mall lab1 to pass eslint

--- results/results.js
+++ results/results.js
@@ -1,17 +1,10 @@
 
 import tableCreate from './table.js';
-import arrayData from '../assets/immageData.js';
 
 
 let td = document.getElementById('table');
 
-let name = [];
-let voteCoiceCount = [];
-let seenItemCount = []; 
-
 let questInfo = getItemsFromJSON();
-getChartData(questInfo)
- 
 putInTable(questInfo, td);
 
 
@@ -35,34 +28,3 @@ function putInTable(ansers, td){
 }
 
 
-function clearLocalData(){
-    localStorage.removeItem('VOTES');
-}
-
-function getChartData(arrayI)
-{
-    for (let i = 0; i < arrayI.length; i++) {
-        voteCoiceCount.push(arrayI[i].votes);
-        seenItemCount.push(arrayI[i].timesViewed)
-        name.push(gitName(arrayI[i].id))
-    }
-    console.log(voteCoiceCount);
-    console.log(seenItemCount);
-    console.log(name);
-
-}
-
-function gitName(nameId)
-{
-    let name = ' ';
-    for (let i = 0; i < arrayData.length; i++) { 
-        
-        if (arrayData[i].id === nameId.id){
-            name = arrayData[i].name;
-            return name;
-        }
-    }
-    return null;
-    
-
-}
