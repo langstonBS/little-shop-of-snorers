cb782daf42a8b09208caa5654aace5d01d23d70a
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-27 08:11:25 -0700

    new comit for lab 12

--- results/results.js
+++ results/results.js
@@ -39,12 +39,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: [questInfo.id],
+        labels: [],
         datasets: [{
-            label: 'My First dataset',
+            label: 'voteItem',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [questInfo]
         }]
     },
 
